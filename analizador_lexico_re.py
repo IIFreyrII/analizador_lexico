177b25cfb3d666ba739c92d8cff52448aef14a1b
Bryan Freyre <freyrecanbryan@gmail.com>
2026-09-14 12:44:47 -0600

    Escritura en json finalizada, falta adaptar la salida para que su formato sea correcto

--- analizador_lexico_re.py
+++ analizador_lexico_re.py
@@ -1,14 +1,16 @@
 import re
+import json
 
-codigo = """x1das = 5 + 3 / a1fk
-X = 2"""
+codigo = """x1das = 5 + 3 \n/ a1fk
+X1 = 2"""
 
 # Diccionario de tokens con regex
 especificacion_tokens = {
     'ESPACIO':      '[ \t]+',               #Así se especifican espaciones según la documentación de Python, el + es para que acepte 1 o más veces
+    'SALTO_LINEA':  '[ \n]+',               #Así se especifican saltos de línea según la documentación de Python, el + es para que acepte 1 o más veces
     'COMENTARIO':    '#.*',                 #El punto es para que acepte cualquier caracter, y el asterisco es para que acepte 0 o más veces después del sharp
     'ID':         r'[a-zA-Z]1[a-zA-Z0-9]*',
-    'NUM':        '[0-9]+',
+    'NUM':        r'[\d]+',
     'OP_EQUAL':   '=',
     'OP_ADD':     '[+]',
     'OP_SUB':     '-',
@@ -33,15 +35,17 @@ def analizar(codigo):
             if coincidencia:
                 valor = coincidencia.group()
 
-                if tipo in ('ESPACIO', 'COMENTARIO'):
-                    # Se ignoran
-                    columna += len(valor)
-
-                elif "\n" in codigo:
-                    linea += len(valor)
-                    columna = 1
+                if tipo in ('ESPACIO', 'COMENTARIO', 'SALTO_LINEA'):
+                    # Si capturamos un salto de línea, actualizamos el contador de líneas
+                    if '\n' in valor:
+                        linea += valor.count('\n')
+                        # Reiniciamos la columna
+                        columna = len(valor) - valor.rfind('\n')
+                    else:
+                        columna += len(valor)
 
                 else:
+                    # Si no es espacio ni salto ni comentario, SÍ es un token válido
                     tokens.append({
                         'Type': tipo,
                         'Value': valor,
@@ -61,7 +65,23 @@ def analizar(codigo):
     tokens.append({'Type': 'EOF', 'Value': None, 'Line': linea, 'Col': columna})
     return tokens
 
+# def read_file():
+#     with open("analizar.txt", "r", encoding="utf-8") as archivo:
+#         for numero_linea, linea in enumerate (archivo, start=1):
+#             print(f"Análisis de linea {numero_linea}:")
+#             resultado = analizar(codigo)
+#             for token in resultado:
+#                 print(token)
+
+def write_file():
+    with open("resultado.json", "w", encoding="utf-8") as archivo:
+        for token in resultado:
+            archivo.write(f"{token}\n")
+
+
 if __name__ == '__main__':
     resultado = analizar(codigo)
     for token in resultado:
         print(token)
+    # read_file()
+    write_file()
